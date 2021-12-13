91ccf4e3e6dd45b930a379203ed0a00385a33b00
CHAN JUN PARK <wns0415@gmail.com>
2021-12-13 22:37:47 +0900

    Add BOJ 1011, BOJ 15649

--- Algorithm/BOJ15649. N과 M(1).py
+++ Algorithm/BOJ15649. N과 M(1).py
@@ -1,9 +1,27 @@
+def permutation(n, r, st):
+    if r <= 1:
+        for i in n:
+            if i not in st:
+                st.append(i)
+                temp = st.copy()
+                answers.append(temp)
+                st.pop()
 
-def permutation(n, r):
-    for i in n:
+    else:
+        for number in n:
+            if number not in st:
+                st.append(number)
+                permutation(n, r-1, st)
+                st.pop()
+    return answers
 
-    return 0
 
 n, r = map(int, input().split(" "))
 n = [number for number in range(1,n+1)]
-print(permutation(n, r))
+answers = []
+st = []
+permutation(n, r, st)
+for answer in answers:
+    for number in answer:
+        print(number, end=' ')
+    print()
